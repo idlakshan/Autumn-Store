32ba6725e4a0c7bf04a9c93157c1e2cd1cd46272
idlakshan <id.lakshan21@gmail.com>
2024-08-16 11:57:07 +0530

    feat: add product context and fetch products from fakeStore

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+//product context provider 
+import ProductProvider from './context/ProductContext.jsx'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <ProductProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </ProductProvider>
+
+
 )
