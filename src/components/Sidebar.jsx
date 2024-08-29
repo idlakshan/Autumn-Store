d928b9f3fe6b393a8b35929ba675fa55dbbaed34
idlakshan <id.lakshan21@gmail.com>
2024-08-29 09:50:33 +0530

    feat: add hero component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,6 +4,7 @@ import { IoMdArrowForward } from 'react-icons/io';
 import { CartContext } from '../context/CartContext';
 import CartItem from './CartItem';
 import { FiTrash2 } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
@@ -19,15 +20,17 @@ const Sidebar = () => {
           <IoMdArrowForward className='text-2xl'/>
         </div>
       </div>
-      <div className='flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b'>{cart.map((item)=>{
+      <div className='flex flex-col gap-y-2 h-[520px] lg:h-[450px] overflow-y-auto overflow-x-hidden border-b'>{cart.map((item)=>{
         return <CartItem item={item} key={item.id}/>
       })}</div>
 
-      <div className='flex flex-col gap-y-3 py-4 mt-4' >
+      <div className='flex flex-col gap-y-3 py-4 mt-4'>
         <div className='flex w-full justify-between items-center'>
           <div className='uppercase font-semibold'><span className='mr-2'>Total:</span>$ {parseFloat(total).toFixed(2)}</div>
           <div onClick={clearCart} className='cursor-pointer py-4 bg-red-500 text-white w-12 h-12 flex justify-center items-center text-xl'><FiTrash2/></div>
         </div>
+        <Link to={"/"} className='bg-gray-200 flex p-4 justify-center items-center text-primary w-full font-medium'>View Cart</Link>
+        <Link to={"/"} className='bg-primary flex p-4 justify-center items-center text-white w-full font-medium'>Checkout</Link>
       </div>
     </div>
   )
