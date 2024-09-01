eb6e2a66592fe944617381bf0dcc2be1cab2412e
idlakshan <id.lakshan21@gmail.com>
2024-09-01 14:02:14 +0530

    feat: add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+   <footer className='bg-primary py-12'>
+    <div className='container mx-auto'>
+        <p className='text-white text-center'>Copyright &copy; Ecommerce shop 2024. All rights reserved. </p>
+    </div>
+
+   </footer>
   )
 }
 
